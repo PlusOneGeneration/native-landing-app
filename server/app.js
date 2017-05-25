bd43a707443e4dd19822299d57da5ee28b8ad529
Irina Drozdova <irina.drozdova.work@gmail.com>
2017-05-25 17:25:24 +0300

    - wip

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let google = require('googleapis');
+
+let sheets = google.sheets('v4');
 let GoogleAuthService = require("./GoogleAuthService");
 
+
+let spreadsheetId = '1IfWAyHEOz9M0dJn4G1NqshQAvy6RFT8ydlHwJRAu5TA';
 let app = express();
 
 app.use(bodyParser.json());
@@ -10,15 +14,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/registration', function (req, res) {
     GoogleAuthService.authenticate().then((auth) => {
-        getData(auth);
+        addDataToSheet(auth, req.body);
     });
 });
 
 function getData(auth) {
-    let sheets = google.sheets('v4');
     sheets.spreadsheets.values.get({
         auth: auth,
-        spreadsheetId: '1IfWAyHEOz9M0dJn4G1NqshQAvy6RFT8ydlHwJRAu5TA',
+        spreadsheetId: spreadsheetId,
         range: 'Sheet1!A1', //Change Sheet1 if your worksheet's name is something else
     }, (err, response) => {
         if (err) {
@@ -45,7 +48,23 @@ function getData(auth) {
     });
 }
 
-
+function addDataToSheet(auth, data) {
+    sheets.spreadsheets.values.append({
+        auth: auth,
+        spreadsheetId: spreadsheetId,
+        range: 'Sheet1!A1:D', //Change Sheet1 if your worksheet's name is something else
+        valueInputOption: "USER_ENTERED",
+        resource: {
+            values: [[data.first, data.last, data.email, data.message]]
+        }
+    }, (err, response) => {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+        } else {
+            console.log("Appended");
+        }
+    });
+}
 app.listen(3000, function () {
     console.log('Server was started');
 });
