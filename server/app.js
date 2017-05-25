1b9839f0418853c92d4cfa951a7c302258ef82b5
Irina Drozdova <irina.drozdova.work@gmail.com>
2017-05-25 16:55:09 +0300

    - created GoogleAuthService - added google_credentials - updated registration. Try get info from google sheet

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let google = require('googleapis');
+let GoogleAuthService = require("./GoogleAuthService");
 
 let app = express();
 
@@ -7,9 +9,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/registration', function (req, res) {
-    console.log('reg.body', req.body);
-    res.json({text: 'Hello World'});
+    GoogleAuthService.authenticate().then((auth) => {
+        getData(auth);
+    });
 });
+
+function getData(auth) {
+    let sheets = google.sheets('v4');
+    sheets.spreadsheets.values.get({
+        auth: auth,
+        spreadsheetId: '1IfWAyHEOz9M0dJn4G1NqshQAvy6RFT8ydlHwJRAu5TA',
+        range: 'Sheet1!A1', //Change Sheet1 if your worksheet's name is something else
+    }, (err, response) => {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return;
+        }
+
+        let rows = response.values;
+
+        console.log('response get data', response);
+
+        if (!rows) {
+            !console.log('ma!');
+        }
+
+        if (rows.length === 0) {
+            console.log('No data found.');
+        } else {
+            for (let i = 0; i < rows.length; i++) {
+                let row = rows[i];
+                console.log(row.join(", "));
+            }
+        }
+    });
+}
+
 
 app.listen(3000, function () {
     console.log('Server was started');
