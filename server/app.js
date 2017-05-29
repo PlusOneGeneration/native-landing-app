8d95a0d0b6802f3f647d0e0c4129e238eb67d49e
Irina Drozdova <irina.drozdova.work@gmail.com>
2017-05-29 11:44:19 +0300

    - implemented heroku

--- server/app.js
+++ server/app.js
@@ -8,6 +8,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 require('./api/google-sheet')(app);
 
-app.listen(3000, function () {
-    console.log('Server was started');
+let server_port = process.env.PORT || 3000;
+let server_host = process.env.HOST || '0.0.0.0';
+
+app.listen(server_port, server_host, function() {
+    console.log('Server was started on port %d and on server %d', server_port, server_host);
 });
