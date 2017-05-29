385039d066d19171070a3af9328e273a56bd4785
Irina Drozdova <irina.drozdova.work@gmail.com>
2017-05-29 16:12:21 +0300

    - updated UI for phone

--- landing-app/app/resources/registration-resource.ts
+++ landing-app/app/resources/registration-resource.ts
@@ -5,8 +5,8 @@ import 'rxjs/Rx';
 
 @Injectable()
 export class RegistrationResource {
-    private registrationUrl: string = 'http://10.0.2.2:3000/registration';
-    // private registrationUrl: string = ' https://landing-app-server.herokuapp.com/registration';
+    // private registrationUrl: string = 'http://10.0.2.2:3000/registration';
+    private registrationUrl: string = 'https://landing-app-server.herokuapp.com/registration';
 
     constructor(private http: Http) {
     }
